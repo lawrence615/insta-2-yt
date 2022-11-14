f7b15c317941688b9cab9bbdf6283d1795cf5d5a
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-11-14 06:55:50 +0300

    The stories is done, we now have something that we can see and interact with

---
+++ components/Stories.js
@@ -0,0 +1,39 @@
+import { faker } from "@faker-js/faker";
+import { useEffect, useState } from "react";
+import Story from "./Story";
+
+function Stories() {
+  const [suggestions, setSuggestions] = useState([]);
+
+  // code here will run once in the lifecycle of the component
+  useEffect(() => {
+    const suggestions = [...Array(20)].map((_, i) => ({
+      id: i,
+      name: faker.name.fullName(),
+      address: faker.address.streetAddress(),
+      phone: faker.phone.number(),
+      avatar: faker.internet.avatar(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      website: faker.internet.domainName(),
+      dob: faker.date.birthdate(),
+      company: faker.company.name(),
+    }));
+
+    // console.log("suggestions", suggestions);
+    setSuggestions(suggestions);
+  }, []);
+  return (
+    <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
+      {suggestions.map((profile) => (
+        <Story
+          key={profile.id}
+          img={profile.avatar}
+          username={profile.username}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Stories;
