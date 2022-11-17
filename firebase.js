a27457b9aa05d6b34f7e6434096fe133f65cb02c
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-11-17 09:18:05 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,12 +8,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration  
 const firebaseConfig = {
-  apiKey: "AIzaSyAaqFoSeP_1cALnybj64VjdOR9HrR_-7Js",
-  authDomain: "insta-2-yt-50747.firebaseapp.com",
-  projectId: "insta-2-yt-50747",
-  storageBucket: "insta-2-yt-50747.appspot.com",
-  messagingSenderId: "523764792682",
-  appId: "1:523764792682:web:4da5d9b1ccd213bc08b913",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
