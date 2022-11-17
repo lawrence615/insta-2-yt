4200eb0206388734c50f2b4ad6bab092cafc5197
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-11-17 05:57:21 +0300

    nextAuth session wraps the entire app. At this point, we should be able to tap info of the logged in user.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+/**
+ *
+ * SessionProvider - allows us to keep our session state surround the app.
+ *
+ *
+ * @param {*} param0
+ * @returns
+ */
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
