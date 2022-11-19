7d34ca7d10c1a453e69fe80b21ef2ca55fcc90b5
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-11-19 06:01:31 +0300

    the entire app is now wrapped under Recoil

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from "recoil";
 
 /**
  *
@@ -12,7 +13,9 @@ import { SessionProvider } from "next-auth/react";
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
     </SessionProvider>
   );
 }
